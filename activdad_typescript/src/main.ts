aea907a07af0aaef9f6ff1d9e2b47601d43c19c3
Estefanyq19 <estefanyq1908@gmail.com>
2024-09-18 16:42:19 -0600

    Importando la clase en main.ts

---
+++ activdad_typescript/src/main.ts
@@ -0,0 +1,5 @@
+import { cabeceraPag } from "./ejercicio1Cabecera";
+
+const cabecera = new cabeceraPag("Hola Kenia", "Morado", "Time New Roman");
+cabecera.alineacionCabecera('derecha');
+cabecera.imprimirPropiedades();
